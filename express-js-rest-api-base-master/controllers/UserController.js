988800849565ce1c7e4e1e4df94ff904726ee738
Messiaslogia <messias.logia@gmail.com>
2023-11-06 17:52:18 -0300

    Seção 23: [Projeto #05] API Rest de Usuários com Knex.js e JWT

---
+++ express-js-rest-api-base-master/controllers/UserController.js
@@ -0,0 +1,77 @@
+var User = require("../models/User")
+class UserController{
+
+    async index(req, res){
+        var users = await User.findAll();
+        res.json(users);
+    }
+
+    async findUser(req, res){
+        var id = req.params.id
+        var user = await User.findById(id);
+        if(user == undefined){
+            res.status(404);
+            res.json({});
+        }else{
+            res.status(200);
+            res.json(user)
+        }
+    }
+
+    async create(req, res){
+        var {email, name, password} = req.body;
+
+        if(email == undefined){
+            res.status(400);
+            res.json({err: "O e-mail é inválido"})
+            return
+        }
+
+        var emailExists = await User.findEmail(email)
+
+        if(emailExists){
+            res.status(406);
+            res.json({err: "O e-mail já está cadastrado!"})
+            return
+        }
+
+        await User.new(email,password,name)
+
+        res.status(200);
+        res.send("Tudo OK!")
+    }
+
+    async edit(req, res){
+        var {id, name, role, email} = req.body;
+        var result = await User.update(id, email, name, role);
+        if(result != undefined){
+            if(result.status){
+                res.status(200);
+                res.send("Tudo Ok!")
+            }else{
+                res.status(406);
+                res.send(result.err);
+            }
+        }else{
+            res.status(406);
+            res.send("Ocorreu um erro no servidor!")
+        }
+    }
+
+    async remove(req, res){
+        var id = req.params.id;
+
+        var result = await User.delete(id);
+
+        if(result.status){
+            res.status(200);
+            res.send("Tudo OK")
+
+        }else{
+            res.status(406);
+            res.send(result.err)
+        }
+    }
+}
+
+module.exports = new UserController();
--- knex/index.js
+++ knex/index.js
@@ -18,143 +18,143 @@ var dados = [
 ]
 
 // //  INSERT
-// database.insert(dados).into("games").then(data =>{
-//     console.log(data);
-// }).catch(err => {
-//     console.log(err)
-// })
+database.insert(dados).into("games").then(data =>{
+    console.log(data);
+}).catch(err => {
+    console.log(err)
+})
 
 // // SELECT
-// database.select().table("games").then(data =>{
-//     console.log(data);
-// }).catch(err => {
-//     console.log(err);
-// });
+database.select().table("games").then(data =>{
+    console.log(data);
+}).catch(err => {
+    console.log(err);
+});
 
 // // NESTED QUERIES
-// database.insert({nome: "nome do jogo", preco: 25}).into("games").then(data =>{
-//     database.select(["id", "preco"]).table("games").then(data => {
-//         console.log(data);
-//     }).catch(err => {
-//         console.log(err);
-//     });
-// }).catch(err =>{
-//     console.log(err);
-// });
+database.insert({nome: "nome do jogo", preco: 25}).into("games").then(data =>{
+    database.select(["id", "preco"]).table("games").then(data => {
+        console.log(data);
+    }).catch(err => {
+        console.log(err);
+    });
+}).catch(err =>{
+    console.log(err);
+});
 
 // // WHERE
-// var query = database.select(["id", "preco"])
-//     .where({nome: "Mists of noyah"})
-//     .where({id: 2})
-//     .table("games")
+var query = database.select(["id", "preco"])
+    .where({nome: "Mists of noyah"})
+    .where({id: 2})
+    .table("games")
 
 // // OR in WHERE
-// var query = database.select(["id", "preco"])
-//     .where({ nome: "Mists of noyah" })
-//     .orWhere({ id: 2 })
-//     .table("games")
+var query = database.select(["id", "preco"])
+    .where({ nome: "Mists of noyah" })
+    .orWhere({ id: 2 })
+    .table("games")
 
 // // LIKE, !, > tem que ser descrito em linha
-// var query = database.select(["id", "preco"])
-//     .whereRaw("nome = 'Mists of Noyah' OR preco > 120")
-//     .table("games").then(data => {
-//         console.log(data)
-//     }).catch(err =>{
-//         console.log(err)
-//     })
+var query = database.select(["id", "preco"])
+    .whereRaw("nome = 'Mists of Noyah' OR preco > 120")
+    .table("games").then(data => {
+        console.log(data)
+    }).catch(err =>{
+        console.log(err)
+    })
 
 // // RAW
-// database.raw("SELECT * FROM games").then(data =>{
-//         console.log(data)
-//     }).catch(err =>{
-//         console.log(err)
-// })
+database.raw("SELECT * FROM games").then(data =>{
+        console.log(data)
+    }).catch(err =>{
+        console.log(err)
+})
 
 // // DELETE
-// database.where({id: 3}).delete().table("games").then(data => {
-//     console.log(data)
-// }).catch(err => {
-//     console.log(err)
-// })
+database.where({id: 3}).delete().table("games").then(data => {
+    console.log(data)
+}).catch(err => {
+    console.log(err)
+})
 
 // // UPDATE
-// database.where({id:5}).update({preco: 40}). table("games").then(data => {
-//     console.log(data);
-// }).catch(err => {
-//     console.log(err);
-// })
+database.where({id:5}).update({preco: 40}). table("games").then(data => {
+    console.log(data);
+}).catch(err => {
+    console.log(err);
+})
 
 // // ORDER BY
-// database.select().table("games").orderBy("nome","desc").then(data => {
-//     console.log(data);
-// }).catch(err => {
-//     console.log(err);
-// })
+database.select().table("games").orderBy("nome","desc").then(data => {
+    console.log(data);
+}).catch(err => {
+    console.log(err);
+})
 
 ////////////////////////////////////////////////////////////////////////////
 
 // ASSOCIAÇÕES INSERIDAS
-// database.insert({
-//     nome: "Cuspida",
-//     game_id: 5
-// }).table("estudios").then(data =>{
-//     console.log(data);
-// }).catch(err => {
-//     console.log(err);
-// });
+database.insert({
+    nome: "Cuspida",
+    game_id: 5
+}).table("estudios").then(data =>{
+    console.log(data);
+}).catch(err => {
+    console.log(err);
+});
 
 // // RELACIONAMENTO 1 PARA 1 
-// database.select().table("games").innerJoin("estudios", "estudios.game_id", "games.id").then( data =>{
-//     console.log(data)
-// }).catch(err => {
-//     console.log(err)
-// })
+database.select().table("games").innerJoin("estudios", "estudios.game_id", "games.id").then( data =>{
+    console.log(data)
+}).catch(err => {
+    console.log(err)
+})
 
 // // RELACIONAMENTO 1 PARA 1 
-// database.select("games.id", "estudios.id as estudio_id", "games.nome as game_nome", "estudios.nome as estudio_nome").table("games").innerJoin("estudios", "estudios.game_id", "games.id").then(data => {
-//     console.log(data)
-// }).catch(err => {
-//     console.log(err)
-// })
+database.select("games.id", "estudios.id as estudio_id", "games.nome as game_nome", "estudios.nome as estudio_nome").table("games").innerJoin("estudios", "estudios.game_id", "games.id").then(data => {
+    console.log(data)
+}).catch(err => {
+    console.log(err)
+})
 
 // // RELACIONAMENTO 1 PARA 1 com where
-// database.select("games.id", "estudios.id as estudio_id", "games.nome as game_nome", "estudios.nome as estudio_nome").table("games").innerJoin("estudios", "estudios.game_id", "games.id").where("games.id",1).then(data => {
-//     console.log(data)
-// }).catch(err => {
-//     console.log(err)
-// })
+database.select("games.id", "estudios.id as estudio_id", "games.nome as game_nome", "estudios.nome as estudio_nome").table("games").innerJoin("estudios", "estudios.game_id", "games.id").where("games.id",1).then(data => {
+    console.log(data)
+}).catch(err => {
+    console.log(err)
+})
 
 // RELACIONAMENTO 1 PARA M
-// database.select("games.*", "estudios.nome as estudio_nome").table("games").innerJoin("estudios", "estudios.game_id", "games.id").where("games.id",5).then(data => {
+database.select("games.*", "estudios.nome as estudio_nome").table("games").innerJoin("estudios", "estudios.game_id", "games.id").where("games.id",5).then(data => {
     
-//     var estudiosGames = data;
-//     var games = {
-//         id: 0,
-//         nome: " ",
-//         estudios: []
-//     }
+    var estudiosGames = data;
+    var games = {
+        id: 0,
+        nome: " ",
+        estudios: []
+    }
 
-//     games.id = data[0].id;
-//     games.nome = data[0].nome;
+    games.id = data[0].id;
+    games.nome = data[0].nome;
 
-//     data.forEach(estudio => {
-//         games.estudios.push({nome: estudio.estudio_nome});
-//     });
-//     console.log(games)
-// }).catch(err => {
-//     console.log(err)
-// })
+    data.forEach(estudio => {
+        games.estudios.push({nome: estudio.estudio_nome});
+    });
+    console.log(games)
+}).catch(err => {
+    console.log(err)
+})
 
 // RELACIONAMENTO M PARA M
-// database.select(["estudios.nome as estudio_nome", "games.nome as game_nome", "games.preco"])
-//     .table("games_estudios")
-//     .innerJoin("games", "games.id", "games_estudios.estudio_id")
-//     .innerJoin("estudios", "games.id", "games_estudios.estudio_id")
-//     .where("estudios.id",2).then(data =>{
-//         console.log(data);
-//     }).catch(err =>{
-//         console.log(err)
-// })
+database.select(["estudios.nome as estudio_nome", "games.nome as game_nome", "games.preco"])
+    .table("games_estudios")
+    .innerJoin("games", "games.id", "games_estudios.estudio_id")
+    .innerJoin("estudios", "games.id", "games_estudios.estudio_id")
+    .where("estudios.id",2).then(data =>{
+        console.log(data);
+    }).catch(err =>{
+        console.log(err)
+})
 
 
 async function testeTransacao(){
